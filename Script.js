6f61daf8facf0a83a143606f431ce17dadb4672e
Bruno <brunonicolau131@gmail.com>
2025-03-28 08:31:20 -0300

    Projeto versão final, falta os scripts e acabar o design

---
+++ Script.js
@@ -0,0 +1,36 @@
+function galeria() {
+    window.location.href = 'galeria.html';
+}
+
+function sobre() {
+    window.location.href = 'Index.html';
+}
+
+function Motivos() {
+    window.location.href = 'Motivos.html';
+}
+
+function Equipe() {
+    window.location.href = 'Equipe.html';
+}
+
+function Arcade() {
+    window.location.href = 'Arcade.html';
+}
+
+function Estrela() {
+    window.location.href = 'Estrela.html';
+}
+
+function Processo() {
+    window.location.href = 'Processocriacao.html';
+}
+
+function Agradecimento() {
+    window.location.href = 'Agradecimento.html';
+}
+
+
+function Games() {
+    window.location.href = 'Games.html';
+}
