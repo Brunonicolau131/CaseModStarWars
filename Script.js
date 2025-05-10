4eea0b8857dbe7fb2453be11cce732e6e82b4fbd
Maicon Luis Silva <maiconlsilva100@gmail.com>
2025-05-10 20:29:01 -0300

    - Fotos dos professores adicionadas em agradecimentos.html - Estilo .galeria img ajustado para largura e altura fixas

--- Script.js
+++ Script.js
@@ -50,15 +50,15 @@ const mensagemImagem = document.getElementById("mensagemImagem");
 const mensagens = {
     "Unifecaf.jpg": "Nossa gratidão à UNIFecaf por oferecer um ambiente educacional que estimula a inovação e a prática. A estrutura, os recursos e o apoio institucional foram fundamentais para a realização deste projeto, que representa um marco importante na nossa formação acadêmica.",
     
-    "2.jpg": "Agradecemos ao Professor Rodrigo Moreira dos Santos, da disciplina de Cyber Security, por compartilhar seu vasto conhecimento e nos guiar com excelência nas práticas de segurança da informação. Sua orientação foi essencial para a implementação eficaz do firewall e da proteção da nossa rede.",
+    "Rodrigo.png": "Agradecemos ao Professor Rodrigo Moreira dos Santos, da disciplina de Cyber Security, por compartilhar seu vasto conhecimento e nos guiar com excelência nas práticas de segurança da informação. Sua orientação foi essencial para a implementação eficaz do firewall e da proteção da nossa rede.",
 
-    "3.jpg": "Nosso reconhecimento ao Professor Nilvan Garcia de Almeida, responsável pela disciplina de Server and Data Center Administration, por seu suporte técnico e pedagógico na criação e configuração do servidor. Seu ensinamento foi crucial para compreendermos a importância da administração de servidores em ambientes profissionais.",
+    "Nilvan.png": "Nosso reconhecimento ao Professor Nilvan Garcia de Almeida, responsável pela disciplina de Server and Data Center Administration, por seu suporte técnico e pedagógico na criação e configuração do servidor. Seu ensinamento foi crucial para compreendermos a importância da administração de servidores em ambientes profissionais.",
 
-    "4.jpg": "Agradecemos ao Professor Robinson Lemos Bezerra, da disciplina de Computer Network, por nos proporcionar uma base sólida sobre redes de computadores. Seu apoio foi determinante para a estruturação e configuração da rede utilizada em nosso projeto.",
+    "Robinson.png": "Agradecemos ao Professor Robinson Lemos Bezerra, da disciplina de Computer Network, por nos proporcionar uma base sólida sobre redes de computadores. Seu apoio foi determinante para a estruturação e configuração da rede utilizada em nosso projeto.",
 
-    "5.jpg": "Nossa gratidão ao Professor Robson Rodrigues da Silva, da disciplina de Computer Architecture and Organization, por nos orientar com clareza sobre os fundamentos de hardware e arquitetura. Seu ensinamento foi essencial para a montagem do nosso casemod e para a integração entre os diferentes componentes do projeto.",
+    "Robson0.png": "Nossa gratidão ao Professor Robson Rodrigues da Silva, da disciplina de Computer Architecture and Organization, por nos orientar com clareza sobre os fundamentos de hardware e arquitetura. Seu ensinamento foi essencial para a montagem do nosso casemod e para a integração entre os diferentes componentes do projeto.",
 
-    "6.jpg": "Agradecemos imensamente aos integrantes da nossa equipe — Bruno, Fabia, Kayke, Maicon e Rhafael — pelo comprometimento, dedicação e colaboração em todas as etapas deste projeto. Cada contribuição foi essencial para que conseguíssemos transformar ideias em resultados concretos, unindo conhecimento técnico e trabalho em equipe.!"
+    "equipe.jpg": "Agradecemos imensamente aos integrantes da nossa equipe — Bruno, Fabia, Kayke, Maicon e Rhafael — pelo comprometimento, dedicação e colaboração em todas as etapas deste projeto. Cada contribuição foi essencial para que conseguíssemos transformar ideias em resultados concretos, unindo conhecimento técnico e trabalho em equipe.!"
 };
 
 imagens.forEach(img => {
